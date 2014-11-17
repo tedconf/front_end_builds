e89bfe58039b0117df85ffa34e8515269c625c57
Sam Selikoff <sam.selikoff@gmail.com>
2014-11-17 17:31:21 -0500

    Merge public assets from engine with host application

--- lib/front_end_builds/engine.rb
+++ lib/front_end_builds/engine.rb
@@ -8,5 +8,10 @@ module FrontEndBuilds
       g.assets false
       g.helper false
     end
+
+     # Initializer to combine this engines static assets with the static assets of the hosting site.
+    initializer "static assets" do |app|
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+    end
   end
 end
