371bc108e5947628dba5f0e4d8fb129c8184a2ef
Sam Selikoff <sam.selikoff@gmail.com>
2014-11-24 00:06:35 -0500

    Add builds to app response

--- app/controllers/front_end_builds/apps_controller.rb
+++ app/controllers/front_end_builds/apps_controller.rb
@@ -5,8 +5,25 @@ module FrontEndBuilds
     respond_to :json
 
     def index
-      @apps = FrontEndBuilds::App.all
-      respond_with apps: @apps
+      @apps = []
+      @builds = []
+
+      FrontEndBuilds::App.all.each do |app|
+        # Add this app's builds to @builds
+        @builds.push app.builds
+
+        # Add this app's build IDs to app's hash
+        build_ids = app.builds.map {|build| build.id}
+        app = app.as_json.merge( {build_ids: build_ids} )
+
+        # Add app hash to @apps
+        @apps.push app
+      end
+
+      respond_with ({
+        apps: @apps,
+        builds: @builds
+      })
     end
 
     def create
