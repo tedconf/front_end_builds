89a01cd803a1115a1dc7ae3b001f22f853c4cb0f
Sam Selikoff <sam.selikoff@gmail.com>
2014-12-03 21:32:26 -0500

    Move find_best to app

--- app/models/front_end_builds/app.rb
+++ app/models/front_end_builds/app.rb
@@ -10,5 +10,33 @@ module FrontEndBuilds
     def ensure_api_key!
       self.api_key = SecureRandom.uuid if api_key.blank?
     end
+
+    def find_best_build(params = {})
+      query = {
+        fetched: true
+      }
+
+      if params[:sha]
+        query[:sha] = params[:sha]
+
+      elsif params[:job]
+        query[:job] = params[:job]
+
+      elsif params[:branch]
+        # only force activeness on branch based searched. Anyone who
+        # is searching by sha or build# is probably debugging/testing,
+        # so they would want non active builds
+        query[:branch] = params[:branch]
+        query[:active] = true
+
+      end
+
+      builds
+        .where(query)
+        .limit(1)
+        .order('created_at desc')
+        .first
+
+    end
   end
 end
