00f6e7bc9d15bc40236804c112a01352d39fece0
Sam Selikoff <sam.selikoff@gmail.com>
2015-01-14 15:30:06 -0500

    no hostApp -> app relationship

--- admin/app/models/app.js
+++ admin/app/models/app.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  hostApp: DS.belongsTo('hostApp'),
   builds: DS.hasMany('build'),
 
   name: DS.attr('string', {defaultValue: ''}),
--- admin/app/models/host-app.js
+++ admin/app/models/host-app.js
@@ -1,8 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  apps: DS.hasMany('app'),
-
   name: DS.attr('string'),
   friendlyName: function() {
     return this.get('name').replace('_', ' ').capitalize();
