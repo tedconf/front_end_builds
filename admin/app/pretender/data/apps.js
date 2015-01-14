1488e4869d5dd79a403ba436ce4ed47009387696
Sam Selikoff <sam.selikoff@gmail.com>
2015-01-14 16:10:08 -0500

    update first-time build instructions

--- admin/app/pretender/data/apps.js
+++ admin/app/pretender/data/apps.js
@@ -2,18 +2,18 @@ export default [
   {
     id: 1,
     name: 'blog',
-    api_key: '2313123',
+    api_key: 'f6db3e76-8f6a-4591-9231-28c271aca36b',
     best_build_id: 2,
     build_ids: [1, 2, 3]
   },
   {
     id: 2,
     name: 'accounting-reports',
-    api_key: '2313123',
+    api_key: 'e241cdf3-5b46-49e6-a962-c857a557d470',
   },
   {
     id: 3,
     name: 'crm',
-    api_key: '2313123',
+    api_key: 'eca7ad25-5de4-467d-8a28-6ca2d2f48c55',
   },
 ];
