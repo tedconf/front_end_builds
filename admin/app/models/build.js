ffe96d92a2ba84a750b4960d11120eae2fd75fa3
Sam Selikoff <sam.selikoff@gmail.com>
2015-01-15 01:26:14 -0500

    cleaning up

--- admin/app/models/build.js
+++ admin/app/models/build.js
@@ -6,10 +6,7 @@ export default DS.Model.extend({
   sha: DS.attr('string'),
   job: DS.attr('string'),
   branch: DS.attr('string'),
-  html: DS.attr('string'),
-  fetched: DS.attr('boolean'),
   active: DS.attr('boolean'),
-  endpoint: DS.attr('string'),
   createdAt: DS.attr('date'),
 
   isBest: function() {
--- admin/app/pretender/data/builds.js
+++ admin/app/pretender/data/builds.js
@@ -4,7 +4,8 @@ export default [
     app_id: 1,
     sha: '1bc3gdw',
     job: 1,
-    branch: 'nonmaster'
+    branch: 'nonmaster',
+    created_at: '2015-01-03 00:00:00'
   },
   {
     id: 2,
@@ -12,12 +13,14 @@ export default [
     sha: '83jnbj',
     job: 2,
     branch: 'master',
+    created_at: '2014-12-27 00:00:00'
   },
   {
     id: 3,
     app_id: 1,
     sha: 'jt83ndl',
     job: 3,
-    branch: 'master'
+    branch: 'master',
+    created_at: '2014-12-25 00:00:00'
   },
 ];
