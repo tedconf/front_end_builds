a90adbaa3efe27e26c964daf56b1a31ed63941a4
Sam Selikoff <sam.selikoff@gmail.com>
2015-01-15 01:26:14 -0500

    cleaning up

--- admin/app/models/host-app.js
+++ admin/app/models/host-app.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+
   friendlyName: function() {
     return this.get('name').replace('_', ' ').capitalize();
   }.property('name')
