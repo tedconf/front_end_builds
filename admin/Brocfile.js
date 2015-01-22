b7c777c654f2b96bb42a82e99086f5980442d572
Sam Selikoff <sam.selikoff@gmail.com>
2015-01-22 18:03:58 -0500

    Fix fontawesome stuff

--- admin/Brocfile.js
+++ admin/Brocfile.js
@@ -1,11 +1,18 @@
 /* global require, module */
+var fs   = require('fs');
+var path = require('path');
+var path_join = function(){
+  // fix path with windows back slash with path_join
+  return path.join.apply(this, arguments).replace(/\\/g, '/');
+};
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
   fingerprint: {
     enabled: false
-  }
+  },
+  emberCliFontAwesome: { includeFontAwesomeAssets: false }
 });
 
 // Use `app.import` to add additional libraries to the generated
@@ -26,6 +33,16 @@ app.import("bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.e
 app.import("bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg");
 app.import("bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf");
 app.import("bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff");
+
+// Font Awesome
+var faDir = 'bower_components/font-awesome';
+app.import(faDir + "/fonts/fontawesome-webfont.eot", { destDir: "fonts/front_end_builds" });
+app.import(faDir + "/fonts/fontawesome-webfont.svg", { destDir: "fonts/front_end_builds" });
+app.import(faDir + "/fonts/fontawesome-webfont.ttf", { destDir: "fonts/front_end_builds" });
+app.import(faDir + "/fonts/fontawesome-webfont.woff", { destDir: "fonts/front_end_builds" });
+app.import(faDir + "/fonts/fontawesome-webfont.woff2", { destDir: "fonts/front_end_builds" });
+app.import(faDir + "/fonts/FontAwesome.otf", { destDir: "fonts/front_end_builds" });
+
 
 if (app.env === 'production') {
   app.import('vendor/production-pretender/shim.js', {
