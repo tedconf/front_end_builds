c15162fd7b0f09da07a03409b309e50ae18fd2f0
Sam Selikoff <sam.selikoff@gmail.com>
2015-02-02 12:55:40 -0500

    Assert against admin index html

--- spec/requests/admin_api_requests_spec.rb
+++ spec/requests/admin_api_requests_spec.rb
@@ -2,6 +2,7 @@ describe 'The API for the admin section should work', type: :request do
   it 'should load the admin' do
     get '/frontend-config'
     expect(response).to be_success
+    expect(response.body).to match('<title>Admin</title>')
   end
 
   it "should respond to the admin's API requests" do
