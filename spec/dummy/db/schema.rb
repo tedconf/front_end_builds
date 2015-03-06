f7f6d1a51c0e9c95448a1881d8a1d4cb9e0b7a3a
Ryan Toronto <ryanto@gmail.com>
2015-03-06 16:40:00 -0500

    bump version

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20150224040537) do
 
   create_table "front_end_builds_pubkeys", force: :cascade do |t|
     t.string   "name",       null: false
-    t.string   "pubkey",     null: false
+    t.text     "pubkey",     null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
