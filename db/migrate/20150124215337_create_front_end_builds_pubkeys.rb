7ddd4b8642773e98781bdb59868294c58821e9af
Ryan Toronto <ryanto@gmail.com>
2015-03-06 12:58:08 -0500

    fix column type

--- db/migrate/20150124215337_create_front_end_builds_pubkeys.rb
+++ db/migrate/20150124215337_create_front_end_builds_pubkeys.rb
@@ -2,7 +2,7 @@ class CreateFrontEndBuildsPubkeys < ActiveRecord::Migration
   def change
     create_table :front_end_builds_pubkeys do |t|
       t.string :name, null: false
-      t.string :pubkey, null: false
+      t.text :pubkey, null: false
 
       t.timestamps null: false
     end
